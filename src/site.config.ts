9f338dcf16a9fe723d8f4d1528d573ef7b86db50
the-lord-of-stars <1506894634@qq.com>
2025-11-20 01:35:11 +0000

    update comment system

--- src/site.config.ts
+++ src/site.config.ts
@@ -155,7 +155,7 @@ export const integ: IntegrationUserConfig = {
   waline: {
     enable: true,
     // Server service link
-    server: 'https://astro-theme-pure-waline.arthals.ink/',
+    server: 'https://waline-eight-psi.vercel.app',
     // Refer https://waline.js.org/en/guide/features/emoji.html
     emoji: ['bmoji', 'weibo'],
     // Refer https://waline.js.org/en/reference/client/props.html
